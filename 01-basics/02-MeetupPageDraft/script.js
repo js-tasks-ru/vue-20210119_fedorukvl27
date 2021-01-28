aa64f3600a8542162f4733f74ceedd4f93ec8ce3
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-01-28 17:34:43 +0300

    Создан экземпляр Аpp для вывода информации о митапе

--- 01-basics/02-MeetupPageDraft/script.js
+++ 01-basics/02-MeetupPageDraft/script.js
@@ -1,66 +1,59 @@
 import Vue from './vue.esm.browser.js';
 
-/** URL адрес API */
-const API_URL = 'https://course-vue.javascript.ru/api';
-
-/** ID митапа для примера; используйте его при получении митапа */
 const MEETUP_ID = 6;
-
-/**
- * Возвращает ссылку на изображение митапа для митапа
- * @param meetup - объект с описанием митапа (и параметром meetupId)
- * @return {string} - ссылка на изображение митапа
- */
-function getMeetupCoverLink(meetup) {
-  return `${API_URL}/images/${meetup.imageId}`;
-}
-
-/**
- * Словарь заголовков по умолчанию для всех типов элементов программы
- */
-const agendaItemTitles = {
-  registration: 'Регистрация',
-  opening: 'Открытие',
-  break: 'Перерыв',
-  coffee: 'Coffee Break',
-  closing: 'Закрытие',
-  afterparty: 'Afterparty',
-  talk: 'Доклад',
-  other: 'Другое',
-};
-
-/**
- * Словарь иконок для для всех типов элементов программы.
- * Соответствует имени иконок в директории /assets/icons
- */
-const agendaItemIcons = {
-  registration: 'key',
-  opening: 'cal-sm',
-  talk: 'tv',
-  break: 'clock',
-  coffee: 'coffee',
-  closing: 'key',
-  afterparty: 'cal-sm',
-  other: 'cal-sm',
-};
+const API_URL = 'https://course-vue.javascript.ru/api';
 
 export const app = new Vue({
   el: '#app',
-
-  data: {
-    //
+  data(){
+    return{
+      defaultMeetups: [],
+    };
   },
-
-  mounted() {
-    // Требуется получить данные митапа с API
+  async mounted(){
+    let response = await fetch(`${API_URL}/meetups/${MEETUP_ID}`);
+    let fetchedMeetup = await response.json();
+    this.defaultMeetups.push(fetchedMeetup);
+    console.log(this.meetups)
   },
-
-  computed: {
-    //
+  computed:{
+    meetups(){
+      return this.defaultMeetups.map((meetup)=>({
+        ...meetup,
+        agendaItemIcons : {
+          registration: 'key',
+          opening: 'cal-sm',
+          talk: 'tv',
+          break: 'clock',
+          coffee: 'coffee',
+          closing: 'key',
+          afterparty: 'cal-sm',
+          other: 'cal-sm',
+        },
+        agendaItemTitles : {
+          registration: 'Регистрация',
+          opening: 'Открытие',
+          break: 'Перерыв',
+          coffee: 'Coffee Break',
+          closing: 'Закрытие',
+          afterparty: 'Afterparty',
+          talk: 'Доклад',
+          other: 'Другое',
+        },
+        coverStyle: meetup.imageId 
+        ? {'--bg-url' : `url(${this.getMeetupCoverLink(meetup)})`} 
+        : '',
+        localDate: new Date(meetup.date).toLocaleString(navigator.language,{
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+        })
+      }))
+    }
   },
-
-  methods: {
-    // Получение данных с API предпочтительнее оформить отдельным методом,
-    // а не писать прямо в mounted()
+  methods:{
+    getMeetupCoverLink(meetup) {
+      return `${API_URL}/images/${meetup.imageId}`;
+    },
   },
 });
