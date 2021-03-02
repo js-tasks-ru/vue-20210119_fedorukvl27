02407fece953e121e3d0b059aa3cf8619edc53a9
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-03-02 16:14:48 +0300

    Создан компонент NotFoundPage

--- 04-SPA/03-NotFound/views/NotFoundPage.vue
+++ 04-SPA/03-NotFound/views/NotFoundPage.vue
@@ -1,5 +1,15 @@
-<template></template>
+<template>
+    <div class="page page_404">
+        <h1>
+            Страница не найдена <span>404</span>
+        </h1>
+    </div>
+</template>
 
-<script></script>
+<script>
+export default{
+    name: 'NotFoundPage',
+}
+</script>
 
-<style scoped></style>
+<style scoped src="@/../public/assets/styles/_page_404.css"></style>
