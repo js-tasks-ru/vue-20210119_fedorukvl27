5033938c406d523d8d3454849e5b5a2f4c8878b3
Vasiliy Fedoruk <fedorukvl27@gmail.com>
2021-03-07 19:26:12 +0300

    Блок 8. Создан компонент CounterButton

--- 07-forms/01-CounterButton_last_final/components/CounterButton.vue
+++ 07-forms/01-CounterButton_last_final/components/CounterButton.vue
@@ -1,9 +1,32 @@
 <template>
-  <button></button>
+  <button @click="handleClick">{{count_}}</button>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'CounterButton',
+  props:{
+    count:{
+      type: Number,
+      default: 0,
+    },
+  },
+  data(){
+    return{
+      count_: this.count,
+    }
+  },
+  watch:{
+    count(newValue){
+      this.count_ = newValue;
+    },
+  },
+  methods:{
+    handleClick(){
+      this.$emit('increment', this.count_ +=1);
+    }
+  }
+};
 </script>
 
 <style>
